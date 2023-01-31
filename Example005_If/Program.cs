76f6b83d5ec828efdce0cc9e335487094024b21a
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-01 00:22:48 +0300

    Ex5 if-else

---
+++ Example005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if(username.ToLower() == "Alexey")
+{
+    Console.WriteLine("Ура это же Леха"); 
+}
+else
+{
+    Console.WriteLine("Хаю хай, " + username);
+}
