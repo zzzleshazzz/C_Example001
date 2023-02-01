6390e2faaae92341fa3ede147b98d3b1a271b061
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-01 17:16:22 +0300

    Pract. Ex7

--- TESTFOLDER/Program.cs
+++ TESTFOLDER/Program.cs
@@ -1,23 +1,8 @@
-﻿// Практика. Задача 5
-void FillMas(int[] array, int num)
-{
-    num = -num;
-    int i = 0;
-    while (i < array.Length)
-    {
-        array[i] = num;
-        i++;
-        num++;
-    }
-}
-Console.Write("Введите число: ");
+﻿// Практика. Задача 7
+Console.WriteLine("Введите 3х значное число: ");
 int a = int.Parse(Console.ReadLine()!);
-int[] mas = new int[a * 2 + 1];
-FillMas(mas, a);
-
-int count = 0;
-while (count < mas.Length)
-    {
-        Console.Write($"{mas[count]} ");
-        count++;
-    }
+if (99 < a & a < 1000)
+{
+    Console.WriteLine(a % 10);
+}
+else Console.WriteLine("Введите другое число");
