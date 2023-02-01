40a04ca15c82732d81133107e20a32c1b4263f24
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-01 16:58:07 +0300

    Pract. Ex5

---
+++ TESTFOLDER/Program.cs
@@ -0,0 +1,23 @@
+﻿// Практика. Задача 5
+void FillMas(int[] array, int num)
+{
+    num = -num;
+    int i = 0;
+    while (i < array.Length)
+    {
+        array[i] = num;
+        i++;
+        num++;
+    }
+}
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] mas = new int[a * 2 + 1];
+FillMas(mas, a);
+
+int count = 0;
+while (count < mas.Length)
+    {
+        Console.Write($"{mas[count]} ");
+        count++;
+    }
