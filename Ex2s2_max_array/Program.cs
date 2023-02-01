1684faed3c6941a96beabda8fd56d4a11e8472cb
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-01 14:46:19 +0300

    Ex2s2 Create

---
+++ Ex2s2_max_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int fmax(int[] mas)
+{
+    int result = mas[0];
+    int i = 0;
+    while (i < mas.Length)
+    {
+        if (result < mas[i]) result = mas[i];
+        i++;
+    }
+    return result;
+}
+
+int[] list = { 9, 8, 7, 6, 5, 44, 3, 10, 11, 12, 22, 11 };
+
+Console.WriteLine(fmax(list));
