3d944833d17c7b9eced4b0d5bdd4bcb716332012
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-01 15:27:31 +0300

    Ex2s3 NewProject

---
+++ Ex2s3_indexfind/Program.cs
@@ -0,0 +1,15 @@
+﻿int findex(int[] mas, int find)
+{
+    int index = 0;
+    while(index < mas.Length)
+    {
+        if (mas[index] == find) break;
+        else index++;
+    }
+    return index;
+}
+
+int[] array = { 12, 32, 53, 14, 25, 66, 78, 68, 49 };
+int find = 53;
+
+Console.WriteLine(findex(array, find));
