02f941499233da3125d4a6c468b9e2177ef62f50
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-04 11:32:40 +0300

    Les3Ex19 Created

---
+++ Les3Ex19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n / 10000 == n % 10)
+{
+    if (n / 1000 % 10 == n % 100 / 10) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+else Console.WriteLine("Нет");
