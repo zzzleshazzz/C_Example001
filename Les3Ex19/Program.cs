8cfdd99e72bacd0fc835b7c8799c49b8b2181cd5
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-04 11:58:58 +0300

    Les3Ex19 Update num

--- Les3Ex19/Program.cs
+++ Les3Ex19/Program.cs
@@ -1,8 +1,26 @@
 ﻿Console.Write("Введите число: ");
 int n = int.Parse(Console.ReadLine()!);
-if (n / 10000 == n % 10)
+
+// РЕШЕНИЕ ДЛЯ ЧИСЕЛ ЛЮБОЙ ДЛИНЫ
+
+int a = n;
+int b = 0;
+while (a > 0)
 {
-    if (n / 1000 % 10 == n % 100 / 10) Console.WriteLine("Да");
-    else Console.WriteLine("Нет");
+    b *= 10;
+    b += a % 10;
+    a = a / 10;
 }
+if (n == b) Console.WriteLine("Да");
 else Console.WriteLine("Нет");
+
+
+// РЕШЕНИЕ ТОЛЬКО ДЛЯ 5 ЗНАЧНЫХ ЧИСЕЛ
+
+// if (n / 10000 == n % 10)
+// {
+//     if (n / 1000 % 10 == n % 100 / 10) Console.WriteLine("Да");
+//     else Console.WriteLine("Нет");
+// }
+// else Console.WriteLine("Нет");
+
