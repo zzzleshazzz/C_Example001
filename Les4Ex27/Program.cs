f91ee8ec49aeb6e5918ac1e39cf8cebb3e7ef231
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-07 17:43:39 +0300

    Les4Ex27 Crated

---
+++ Les4Ex27/Program.cs
@@ -0,0 +1,14 @@
+﻿int SumNum(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result+=num%10;
+        num=num/10;
+    }
+    return result;
+}
+
+Console.Write($"Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {a} = {SumNum(a)}");
