cfdd3e4402ecaff10d56ee686f03f90cfc25ff20
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-07 17:37:03 +0300

    Les4Ex25 Crated

---
+++ Les4Ex25/Program.cs
@@ -0,0 +1,15 @@
+﻿int Powe(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i<B; i++)
+    {
+        result*=A;
+    }
+    return result;
+}
+
+Console.Write($"Введите A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write($"Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{a}^{b} = {Powe(a,b)}");
