31ba4df52a2d97967edaca5c5f18aa54c8d3e22f
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-07 19:02:56 +0300

    Les4Ex29 New format Enter

--- Les4Ex29/Program.cs
+++ Les4Ex29/Program.cs
@@ -20,3 +20,52 @@ Console.Write("Введите длину массива: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите элементы массива, каждый с новой строчки: ");
 PrintMas(Mass(a));
+
+
+// Ввод массива одной строкой в формате: "12,1,53,64"
+// int[] Mass(string s)
+// {
+//     int cNum = 1;
+//     string res = "";
+//     int lenS = s.Length;
+//     for (int i = 0; i < lenS; i++)
+//     {   
+//         if (s[i].ToString() == ",") cNum+=1;
+//     }
+//     int[] result = new int[cNum];
+//     int index = 0;
+//     for (int i = 0; i < lenS; i++)
+//     {   
+//         if(i != (lenS-1))
+//         {
+//             if (s[i].ToString() != ",") res += s[i];
+//             else
+//             {
+//             result[index] = int.Parse(res);
+//             index++;
+//             res = "";
+//             }
+//         }
+//         else
+//         {
+//             res += s[i];
+//             result[index] = int.Parse(res);
+//             index++;
+//             res = "";
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintMas(int[] mas)
+// {
+//     for (int i = 0; i < mas.Length; i++)
+//     {
+//         Console.Write($"{mas[i]} ");
+//     }
+// }
+
+// Console.Write("Введите числа для создания массива в формате '1,2,3,4' :");
+// string l = Console.ReadLine()!;
+
+// PrintMas(Mass(l));
