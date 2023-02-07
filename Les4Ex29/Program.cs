fe6d6c982da8423949b4f4c2cc645dfaee769720
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-07 18:10:14 +0300

    Les4Ex29 Crated

---
+++ Les4Ex29/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] Mass(int n)
+{
+    int[] mas = new int[n];
+    for(int i = 0; i<n; i++)
+    {
+        mas[i] = int.Parse(Console.ReadLine()!); 
+    }
+    return mas;
+}
+
+void PrintMas(int[] masiv)
+{
+    for(int i = 0; i<masiv.Length; i++)
+    {
+        Console.Write($"{masiv[i]} ");
+    }
+}
+
+Console.Write("Введите длину массива: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите элементы массива, каждый с новой строчки: ");
+PrintMas(Mass(a));
