e8a503ad051d33fec090727ce94b60e6a9178e9d
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-10 13:26:21 +0300

    Les5Ex36 Created

---
+++ Les5Ex36/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+// Создание и заполнение массива
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+// Сумма нечетных элементов
+int SumOdd(int[] mas)
+{
+    int sumodd = 0;
+    for(int i = 1; i<mas.Length; i+=2)
+    {
+        sumodd+= mas[i];
+    }
+    return sumodd;
+}
+
+int[] array = GetArray(4, 1, 10);
+Console.WriteLine($"Сумма нечетных чисел массива:\n[{string.Join(",", array)}]\nРавна: {SumOdd(array)}");
