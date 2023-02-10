815829d095ea68a52825bfc0eefb62fd485b25c7
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-10 13:17:01 +0300

    Les5Ex34 Created

---
+++ Les5Ex34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Создание и заполнение массива
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+// Количество четных элементов
+int CountEven(int[] mas)
+{
+    int count = 0;
+    for(int i = 0; i < mas.Length; i++)
+    {
+        if(mas[i]%2==0) count++;
+    }
+    return count;
+}
+
+int[] array = GetArray(5, 100, 999);
+Console.WriteLine($"В массиве [{string.Join(",", array)}], чётных элементов: {CountEven(array)}");
