df10faf9dc696b8a4c44cd7022e5850ce0fd664c
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-17 19:19:12 +0300

    Les6Ex42 Created

---
+++ Les6Ex42/Program.cs
@@ -0,0 +1,14 @@
+﻿string ConvertDecToBin(int num)
+{
+    string result = String.Empty;
+    while (num != 0)
+    {
+        result = (num % 2).ToString() + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.Write("Введите десятичное число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Двоичный формат: {ConvertDecToBin(number)}");
