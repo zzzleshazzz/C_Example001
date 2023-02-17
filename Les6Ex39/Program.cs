33d6dc4da0613b89e8c95a0b2b880ba9fd84adca
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-17 18:44:48 +0300

    Les6Ex39 Created

---
+++ Les6Ex39/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] NewArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) array[i] = new Random().Next(minValue, maxValue);
+    return array;
+}
+
+void ReverseArray(int[] mas)
+{
+    int len = mas.Length;
+    for (int i = 0; i < len / 2; i++)
+    {
+        int a = mas[i];
+        mas[i] = mas[len-i-1];
+        mas[len-i-1] = a;
+    }
+}
+
+int[] arr = NewArray(4, 1, 5);
+Console.WriteLine(string.Join(", ", arr));
+ReverseArray(arr);
+Console.WriteLine(string.Join(", ", arr));
