1e4e9114197cbc694620bdabf9ba91652c289e8f
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-17 19:46:35 +0300

    Les6Ex45 Created

---
+++ Les6Ex45/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] CoppyArray(int[] mas)
+{
+    int[] CoppyArr = new int[mas.Length];
+    for(int i = 0; i<mas.Length; i++) CoppyArr[i] = mas[i];
+    return CoppyArr;
+}
+
+int[] Array = GetArray(6, 10, 99);
+Console.WriteLine($"Оригинал (Array): {string.Join(", ", Array)}");
+int[] CoppyArr = CoppyArray(Array);
+Console.WriteLine($"Копия (CoppyArr): {string.Join(", ", Array)}");
