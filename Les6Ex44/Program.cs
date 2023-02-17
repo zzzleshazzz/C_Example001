0cbeb66a625190066b61744a884b5a2b5e1036e2
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-17 19:38:26 +0300

    Les6Ex44 Created

---
+++ Les6Ex44/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] Fibonachi(int num)
+{
+    int[] mas = new int[num];
+    mas[0] = 0;
+    mas[1] = 1;
+    for(int i = 2; i<num;i++)
+    {
+        mas[i]= mas[i-1]+mas[i-2];
+    }
+    return mas;
+}
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write($"{string.Join(", ", Fibonachi(a))}");
