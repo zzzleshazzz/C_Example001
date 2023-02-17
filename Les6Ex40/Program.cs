13b0fa7c6220d997588bec72ee17e6abc9e4872a
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-17 18:55:30 +0300

    Les6Ex40 Created

---
+++ Les6Ex40/Program.cs
@@ -0,0 +1,21 @@
+﻿string BornTreug(int q, int w, int e)
+{
+    if(q<w+e)
+    {
+        if(w<q+e)
+        {
+            if(e<w+q) return "может";
+            else return "не может";
+        }
+        else return "не может";
+    }
+    else return "не может";
+}
+Console.Write("Введите сторону A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите сторону B: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите сторону C: ");
+int c = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Треугольник со сторонами: {a}, {b}, {c} - {BornTreug(a,b,c)} существовать");
