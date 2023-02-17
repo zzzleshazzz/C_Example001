e2d7405329c9bb62bdb75038af18e2b73ed9da56
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-17 21:40:48 +0300

    Les6Ex41 Created

---
+++ Les6Ex41/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int[] CreateArray(int n)
+{
+    int[] newArr = new int[n];
+    for(int i = 0; i<n;i++) newArr[i] = int.Parse(Console.ReadLine()!);
+    return newArr;
+}
+
+int CountPositive(int[] mas)
+{
+    int result = 0;
+    for(int i = 0; i<mas.Length; i++) if(mas[i]>0) result++;
+    return result;
+}
+
+Console.Write("Введите количество чисел: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите числа, каждое с новой строки: ");
+int[] array = CreateArray(M);
+Console.Write($"Количество введенных чисел > 0 = {CountPositive(array)}");
