222bfad78837d7051a41fe06070ad3ef20d5ce4d
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-02-19 12:30:12 +0300

    new version

--- Les6Ex41/Program.cs
+++ Les6Ex41/Program.cs
@@ -1,12 +1,5 @@
 ﻿Console.Clear();
 
-int[] CreateArray(int n)
-{
-    int[] newArr = new int[n];
-    for(int i = 0; i<n;i++) newArr[i] = int.Parse(Console.ReadLine()!);
-    return newArr;
-}
-
 int CountPositive(int[] mas)
 {
     int result = 0;
@@ -14,8 +7,14 @@ int CountPositive(int[] mas)
     return result;
 }
 
-Console.Write("Введите количество чисел: ");
-int M = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите числа, каждое с новой строки: ");
-int[] array = CreateArray(M);
-Console.Write($"Количество введенных чисел > 0 = {CountPositive(array)}");
+void PrintArray(int[] mas)
+{
+    for(int i = 0; i<mas.Length; i++)
+    {
+        Console.Write($"{mas[i]} ");
+    }
+}
+
+Console.Write("Введите числа через пробел:\n");
+int[] array = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();;
+Console.WriteLine($"Количество чисел > 0: {CountPositive(array)}");
