c8fe1bfb13c3a75df53835d60fc043842bc75e74
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-03-01 11:27:33 +0300

    Les9Ex68 Create

---
+++ Les9Ex68(True)/Program.cs
@@ -0,0 +1,12 @@
+﻿int Akerman(int m, int n)
+{
+    if(m==0) return n+1;
+    if(n==0) return Akerman(m-1,1);
+    return Akerman(m-1, Akerman(m,n-1));
+}
+
+Console.Write("Введите M: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Akerman(x, y));
