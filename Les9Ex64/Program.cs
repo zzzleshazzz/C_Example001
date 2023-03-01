5d4aa765b17ef5f1fbe29daea05dc2869f00ef7a
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-03-01 11:26:19 +0300

    Les9Ex64 Create

---
+++ Les9Ex64/Program.cs
@@ -0,0 +1,9 @@
+﻿string NaturalNum(int n)
+{
+    if (n==1) return $"{1}";
+    return $"{n}, "+NaturalNum(n-1);
+}
+
+Console.Write("Введите N: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NaturalNum(num));
