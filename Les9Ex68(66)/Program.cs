1122e9121ab1d4be417548534b2fdcfe69d24878
Alexey Kolbasov <zzzleshazzz@gmail.com>
2023-03-01 11:27:08 +0300

    Les9Ex66 Create

---
+++ Les9Ex68(66)/Program.cs
@@ -0,0 +1,11 @@
+﻿int SumNtoM(int m, int n)
+{
+    if (m==n) return m;
+    return SumNtoM(m+1,n)+m;
+}
+
+Console.Write("Введите M: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNtoM(x, y));
